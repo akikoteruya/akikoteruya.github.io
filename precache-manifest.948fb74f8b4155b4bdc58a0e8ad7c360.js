86f574f95d0d0d4ab78fe90c68572c04653467a0
Nicolas Lefeld <lefeldnicolas@gmail.com>
2021-09-06 20:40:04 -0300

    Updates

---
+++ precache-manifest.948fb74f8b4155b4bdc58a0e8ad7c360.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c6d5f4c4140996179a1970ca93752c5",
+    "url": "/resume/index.html"
+  },
+  {
+    "revision": "155626f2fa2df2697e98",
+    "url": "/resume/static/css/2.b006ff11.chunk.css"
+  },
+  {
+    "revision": "1e59a3b58f4c7a9b3de9",
+    "url": "/resume/static/css/main.f2ed8c86.chunk.css"
+  },
+  {
+    "revision": "155626f2fa2df2697e98",
+    "url": "/resume/static/js/2.a619349d.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "/resume/static/js/2.a619349d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e59a3b58f4c7a9b3de9",
+    "url": "/resume/static/js/main.85707282.chunk.js"
+  },
+  {
+    "revision": "dd2bda9b9b0a5be907dc",
+    "url": "/resume/static/js/runtime-main.a3fd0379.js"
+  },
+  {
+    "revision": "a6dd04a2c48020a8a344efa1c5a0244a",
+    "url": "/resume/static/media/1.a6dd04a2.jpeg"
+  },
+  {
+    "revision": "ed78aa3f85fa004abf1f3d12f13cee93",
+    "url": "/resume/static/media/2.ed78aa3f.jpeg"
+  },
+  {
+    "revision": "b21b2deac93459d89a596eadb61b21bc",
+    "url": "/resume/static/media/3.b21b2dea.jpeg"
+  },
+  {
+    "revision": "71a85704d10d0645dde3d5df8578eb05",
+    "url": "/resume/static/media/4.71a85704.jpeg"
+  },
+  {
+    "revision": "f38b06d6898bf843f535ed59e3a572b5",
+    "url": "/resume/static/media/5.f38b06d6.jpeg"
+  },
+  {
+    "revision": "57b453e089a66b854a85e16515aef627",
+    "url": "/resume/static/media/logo.57b453e0.png"
+  },
+  {
+    "revision": "69ba7a26e18722b13fbefd201a11b1c4",
+    "url": "/resume/static/media/profile.69ba7a26.png"
+  }
+]);
